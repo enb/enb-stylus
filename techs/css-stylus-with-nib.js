6274b45e844659b0e4b0e4508ceb2184d76729d6
Marat Dulin <mdevils@yandex.ru>
2013-12-16 18:17:44 +0400

    Initial release

---
+++ techs/css-stylus-with-nib.js
@@ -0,0 +1,40 @@
+/**
+ * css-stylus-with-nib
+ * ===================
+ *
+ * Собирает *css*-файлы вместе со *styl*-файлами по deps'ам, обрабатывает инклуды и ссылки, сохраняет в виде `?.css`.
+ * При сборке *styl*-файлов использует `nib`.
+ *
+ * **Опции**
+ *
+ * * *String* **target** — Результирующий таргет. По умолчанию `?.css`.
+ * * *Object* **variables** — Дополнительные переменные окружения для `stylus`.
+ * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов
+ *   (его предоставляет технология `files`). По умолчанию — `?.files`.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech(require('enb/techs/css-stylus-with-nib'));
+ * ```
+ */
+
+module.exports = require('./css-stylus').buildFlow()
+    .name('css-stylus-with-nib')
+    .methods({
+        _configureRenderer: function (renderer) {
+            var nib;
+            try {
+                nib = require('nib');
+            } catch (e) {
+                throw new Error(
+                    'The technology "css-stylus-with-nib" cannot be executed ' +
+                    'because the npm module "nib" was not found.'
+                );
+            }
+
+            renderer.use(nib());
+            return renderer;
+        }
+    })
+    .createTech();
---
+++ techs/css-stylus.js
@@ -0,0 +1,75 @@
+/**
+ * css-stylus
+ * ==========
+ *
+ * Собирает *css*-файлы вместе со *styl*-файлами по deps'ам, обрабатывает инклуды и ссылки, сохраняет в виде `?.css`.
+ *
+ * **Опции**
+ *
+ * * *String* **target** — Результирующий таргет. По умолчанию `?.css`.
+ * * *Object* **variables** — Дополнительные переменные окружения для `stylus`.
+ * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов
+ *   (его предоставляет технология `files`). По умолчанию — `?.files`.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech(require('enb/techs/css-stylus'));
+ * ```
+ */
+var Vow = require('vow');
+var stylus = require('stylus');
+
+module.exports = require('enb/techs/css').buildFlow()
+    .name('css-stylus')
+    .defineOption('variables')
+    .useFileList(['css', 'styl'])
+    .builder(function (sourceFiles) {
+        var _this = this;
+        var promise = Vow.promise();
+
+        var css = sourceFiles.map(function (file) {
+            var path = file.fullname;
+            if (file.name.indexOf('.styl') !== -1) {
+                return '/* ' + path + ':begin */\n' +
+                    '@import "' + path + '";\n' +
+                    '/* ' + path + ':end */\n';
+            } else {
+                return '@import "' + path + '";';
+            }
+        }).join('\n');
+
+        var targetName = _this._target;
+        var renderer = stylus(css)
+            .define('url', function (url) {
+                return new stylus.nodes.Literal('url(' + _this._resolveCssUrl(url.val, url.filename) + ')');
+            });
+        if (this._variables) {
+            var variables = this._variables;
+            Object.keys(variables).forEach(function (key) {
+                renderer.define(key, variables[key]);
+            });
+        }
+
+        _this._configureRenderer(renderer)
+            .render(function (err, css) {
+                if (err) {
+                    promise.reject(err);
+                } else {
+                    promise.fulfill(css);
+                }
+            });
+
+        return promise.then(function (css) {
+            return _this._processIncludes(css, _this.node.resolvePath(targetName));
+        });
+    })
+    .methods({
+        _resolveCssUrl: function (data, filename) {
+            return this._getCssPreprocessor()._resolveCssUrl(data, filename);
+        },
+        _configureRenderer: function (renderer) {
+            return renderer;
+        }
+    })
+    .createTech();
