511e31c00cc213da52b2be27b42be7b6dfb4fc61
Marat Dulin <mdevils@yandex.ru>
2013-12-16 19:14:11 +0400

    Style fix

--- techs/css-stylus-with-autoprefixer.js
+++ techs/css-stylus-with-autoprefixer.js
@@ -16,7 +16,10 @@
  * **Пример**
  *
  * ```javascript
- * nodeConfig.addTech(require('enb-stylus/techs/css-stylus-with-autoprefixer'), {autoprefixerArguments: ['ie 7', 'ie 8']});
+ * nodeConfig.addTech(
+ *     require('enb-stylus/techs/css-stylus-with-autoprefixer'),
+ *     {autoprefixerArguments: ['ie 7', 'ie 8']}
+ * );
  * ```
  */
 
