58c6d1caf9c5ccc544b97f424eb9321a61e1bad9
Marat Dulin <mdevils@yandex.ru>
2014-05-26 14:52:10 +0400

    Rename autoprefixerArguments -> browsers

--- techs/css-stylus-with-autoprefixer.js
+++ techs/css-stylus-with-autoprefixer.js
@@ -9,7 +9,7 @@
  *
  * * *String* **target** — Результирующий таргет. По умолчанию `?.css`.
  * * *Object* **variables** — Дополнительные переменные окружения для `stylus`.
- * * *Array* **autoprefixerArguments** — Аргументы для автопрефиксера.
+ * * *Array* **browsers** — Браузеры.
  * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов
  *   (его предоставляет технология `files`). По умолчанию — `?.files`.
  *
@@ -18,7 +18,7 @@
  * ```javascript
  * nodeConfig.addTech(
  *     require('enb-stylus/techs/css-stylus-with-autoprefixer'),
- *     {autoprefixerArguments: ['ie 7', 'ie 8']}
+ *     {browsers: ['ie 7', 'ie 8']}
  * );
  * ```
  */
@@ -27,10 +27,11 @@ var autoprefixer = require('autoprefixer');
 
 module.exports = require('./css-stylus').buildFlow()
     .name('css-stylus-with-autoprefixer')
+    .defineOption('browsers')
     .defineOption('autoprefixerArguments')
     .methods({
         _configureRenderer: function (renderer) {
-            var args = this._autoprefixerArguments;
+            var args = this._browsers || this._autoprefixerArguments;
             renderer.use(function (style) {
                 this.on('end', function (err, css) {
                     return args ?
